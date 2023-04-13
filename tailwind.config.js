8ff21a5450c7160e033f0637294ca9f3c3eb3638
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-04-14 00:00:45 +0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const windmill = require('@windmill/react-ui/config');
 
 module.exports = windmill({
-  purge: ['src/**/*.js'],
+  content: ['src/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
